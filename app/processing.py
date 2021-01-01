7af3befa9f81efc0feb1bb531c36f26223a9dfc8
Blaž Rojc <blaz.rojc.si@gmail.com>
2021-01-01 14:28:05 +0100

    Premaknil alert v ločen proces.

--- app/processing.py
+++ app/processing.py
@@ -1,12 +1,12 @@
 from os import getenv
+from multiprocessing import Process
 import requests
 
 from . import models
 
-async def alert_for_new_sample(token: str, sample: models.Sample) -> bool:
+def alert_separately_sample(token: str, sample: models.Sample, message: str) -> None:
     if sample.wts:
         # dobi vse želje
-        print("cards IP: " + getenv("USER_CARDS_IP") + f"/v1/wishes?card_id={sample.card_id}")
         wishes_req = requests.get(
             getenv("USER_CARDS_IP") + f"/v1/wishes?card_id={sample.card_id}",
             headers = {
@@ -14,46 +14,42 @@ async def alert_for_new_sample(token: str, sample: models.Sample) -> bool:
                 "Authorization": "Bearer " + token
             })
         if wishes_req.status_code != 200:
-            return False # request failed
+            return # request failed
         wishes = wishes_req.json()
         # za vsako željo pošlji message userju
         for wish in wishes:
-            content = (f"User {sample.user_id} just posted a sample of card {sample.card_id} "
-                + f" in a state \"{sample.state}\", contact them for more info.")
             requests.post(
                 getenv("MESSAGES_IP") + "/v1/messages",
-                data = f"{{\"receiver_id\": {wish['user_id']},\"content\": {content}}}",
+                data = f"{{\"receiver_id\": {wish['user_id']},\"content\": \"{message}\"}}",
                 headers = {
                     "accept": "application/json",
                     "Authorization": "Bearer " + token
                 }
             )
 
-async def alert_for_edited_sample(token: str, sample: models.Sample):
-    if sample.wts:
-        # dobi vse želje
-        wishes_req = requests.get(
-            getenv("USER_CARDS_IP") + "/v1/wishes",
-            data = {"card_id": str(sample.card_id)},
-            headers = {
-                "accept": "application/json",
-                "Authorization": "Bearer " + token
-            })
-        if wishes_req.status_code != 200:
-            return False # request failed
-        wishes = wishes_req.json()
-        # za vsako željo pošlji message userju
-        for wish in wishes:
-            content = (f"User {sample.user_id} just edited a sample of card {sample.card_id} "
-                + f" in a state \"{sample.state}\", contact them for more info.")
-            requests.post(
-                getenv("MESSAGES_IP") + "/v1/messages",
-                data = f"{{\"receiver_id\": {wish['user_id']},\"content\": \"{content}\"}}",
-                headers = {
-                    "accept": "application/json",
-                    "Authorization": "Bearer " + token
-                }
-            )
+async def alert_for_new_sample(token: str, sample: models.Sample) -> None:
+    p = Process(
+        target = alert_separately_sample,
+        args = (
+            token,
+            sample,
+            f"User {sample.user_id} just posted a sample of card {sample.card_id} "
+                + f" in a state \"{sample.state}\", contact them for more info."
+        )
+    )
+    p.start()
+
+async def alert_for_edited_sample(token: str, sample: models.Sample) -> None:
+    p = Process(
+        target = alert_separately_sample,
+        args = (
+            token,
+            sample,
+            f"User {sample.user_id} just edited a sample of card {sample.card_id} "
+                + f" in a state \"{sample.state}\", contact them for more info."
+        )
+    )
+    p.start()
 
 async def alert_for_new_wish(*args, **kwargs):
     pass
